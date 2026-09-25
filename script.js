98a8feda79c8051c8a718835186bde5cacf6647b
new28112545now <new28112545@gmail.com>
2026-09-25 16:15:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
-document.getElementById("bookingForm").addEventListener("submit", function(event) {
+const SUPABASE_URL = "https://qbdptsvcklttdsnttbxh.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = sb_publishable_bdQTf_Fa8OrGXs3JxuiAkQ_5d9RlzIr
+"ใส่_PUBLISHABLE_KEY_ของคุณตรงนี้"
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
+
+document.getElementById("bookingForm").addEventListener("submit", async function(event) {
 
     event.preventDefault();
 
     const name = document.getElementById("name").value;
     const phone = document.getElementById("phone").value;
-    const date = document.getElementById("date").value;
-    const time = document.getElementById("time").value;
-    const people = document.getElementById("people").value;
+    const booking_date = document.getElementById("date").value;
+    const booking_time = document.getElementById("time").value;
+    const people = Number(document.getElementById("people").value);
     const note = document.getElementById("note").value;
 
-    const booking = {
-        name: name,
-        phone: phone,
-        date: date,
-        time: time,
-        people: people,
-        note: note
-    };
+    const { error } = await supabaseClient
+        .from("bookings")
+        .insert([
+            {
+                name: name,
+                phone: phone,
+                booking_date: booking_date,
+                booking_time: booking_time,
+                people: people,
+                note: note
+            }
+        ]);
 
-    localStorage.setItem("booking", JSON.stringify(booking));
+    if (error) {
+
+        console.error(error);
+
+        document.getElementById("result").innerHTML =
+            "❌ จองไม่สำเร็จ กรุณาลองใหม่";
+
+        return;
+    }
 
     document.getElementById("result").innerHTML =
         "✅ จองโต๊ะสำเร็จ! ขอบคุณคุณ " + name;
 
     document.getElementById("bookingForm").reset();
+
+    
 });
